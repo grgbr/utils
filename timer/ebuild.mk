e024575fb103fdc976f3f2a1b83676abac038dbe
Grégor Boirie <gregor.boirie@free.fr>
2025-06-02 16:37:45 +0200

    factorize time libraries build logic

--- timer/ebuild.mk
+++ timer/ebuild.mk
@@ -46,10 +46,7 @@ arlibs                          := $(call kconf_enabled, \
                                           ETUX_TIMER_LIST, \
                                           libetux_timer_list.a)
 libetux_timer_list.a-objs       := static/list.o
-libetux_timer_list.a-lots       := static/common.o
-libetux_timer_list.a-lots       += $(call kconf_enabled, \
-                                          ETUX_TRACE, \
-                                          static/trace.o)
+libetux_timer_list.a-lots       := static/builtin.a
 libetux_timer_list.a-cflags     := $(common-cflags)
 libetux_timer_list.a-pkgconf    := $(common-pkgconf)
 
@@ -67,10 +64,7 @@ arlibs                          += $(call kconf_enabled, \
                                           ETUX_TIMER_HEAP, \
                                           libetux_timer_heap.a)
 libetux_timer_heap.a-objs       := static/heap.o
-libetux_timer_heap.a-lots       := static/common.o
-libetux_timer_heap.a-lots       += $(call kconf_enabled, \
-                                          ETUX_TRACE, \
-                                          static/trace.o)
+libetux_timer_heap.a-lots       := static/builtin.a
 libetux_timer_heap.a-cflags     := $(common-cflags)
 libetux_timer_heap.a-pkgconf    := $(common-pkgconf)
 
@@ -89,10 +83,7 @@ arlibs                          += $(call kconf_enabled, \
                                           ETUX_TIMER_HWHEEL, \
                                           libetux_timer_hwheel.a)
 libetux_timer_hwheel.a-objs     := static/hwheel.o
-libetux_timer_hwheel.a-lots     := static/common.o
-libetux_timer_hwheel.a-lots     += $(call kconf_enabled, \
-                                          ETUX_TRACE, \
-                                          static/trace.o)
+libetux_timer_hwheel.a-lots     := static/builtin.a
 libetux_timer_hwheel.a-cflags   := $(common-cflags)
 libetux_timer_hwheel.a-pkgconf  := $(common-pkgconf)
 
